6468024f9874c3bf1775e2b8be7a58dd9a9dd824
Maggie Davidson <jmagdavidson@gmail.com>
2019-04-01 13:54:36 -0400

    Added handling to proj file for duplicate values

--- SI507_project4.py
+++ SI507_project4.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import requests, json
+import requests, json, csv
 from setup import *
 from caching import *
 
@@ -19,19 +19,23 @@ if not baseurl_data:
 # create a BeautifulSoup object with the data
 soup = BeautifulSoup(baseurl_data, "html.parser")
 
-# get the text of the class that contains the list of states in the dropdown and add each state to the database
+# get the text of the class that contains the list of states in the dropdown and add each state to the database if it doesn't already exist
 dropdown =  soup.find('ul', class_='dropdown-menu SearchBar-keywordSearch')
 states_tags = dropdown.find_all('li')
 for tag in states_tags:
+    # get the url to retrieve the state abbreviation and check if the state is already in the db
     url = tag.a['href']
     split_url = url.split('/')
-    new_state = State(State=tag.text,Abbr=split_url[2].upper(),URL='{}{}'.format(BASEURL,url))
-    session.add(new_state)
+    state_exists = session.query(State.Abbr).filter(State.Abbr.like(split_url[2])).all()
+    # if the state doesn't already exist, add it to the db
+    if not state_exists:
+        new_state = State(State=tag.text,Abbr=split_url[2].upper(),URL='{}{}'.format(BASEURL,url))
+        session.add(new_state)
 session.commit()
 
-# for each state, get the text from the url as stored in the db and cache it
+# for each state retrieved from the States table, get the text from the url as stored in the db, cache it, and create a BeautifulSoup object
 states = session.query(State.Id,State.URL).all()
-for state in states[0:5]:
+for state in states:
     url = state[1]
     id = state[0]
     url_data = cache_diction.get(url)
@@ -42,6 +46,10 @@ for state in states[0:5]:
     parks_table = soup.find('ul', id='list_parks')
     parks_tags = parks_table.find_all('li',class_='clearfix')
     for tag in parks_tags:
-        new_park = Park(Name=tag.h3.text,Type=tag.h2.text,Descr=tag.p.text.strip('\n'),Location=tag.h4.text,State=id)
-        session.add(new_park)
+        # check if the park already exists in the db
+        park_exists = session.query(Park.Name).filter(Park.Name.like(tag.h3.text)).all()
+        # if the park doesn't already exist, add it to the db
+        if not park_exists:
+            new_park = Park(Name=tag.h3.text,Type=tag.h2.text,Descr=tag.p.text.strip('\n'),Location=tag.h4.text,State=id)
+            session.add(new_park)
     session.commit()
